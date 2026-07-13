d1273983557ba16d688707419630b98e67ed021c
miku-wwl <569144003@qq.com>
2026-07-13 20:40:23 +1200

    Complete lab 21 dynamic ingress blocks

--- labs/21-dynamic-block-broken/starter/main.tf
+++ labs/21-dynamic-block-broken/starter/main.tf
@@ -27,20 +27,16 @@ resource "aws_security_group" "web" {
   name        = "${var.name_prefix}-web"
   description = "Web access managed from structured input"
 
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  dynamic "ingress" {
+    for_each = var.ingress_rules
 
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      protocol    = "tcp"
+      cidr_blocks = [ingress.value.cidr_block]
+    }
   }
 
   egress {
